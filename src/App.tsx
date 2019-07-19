bb1b93869b0d8a6d3475a85959010eac8a964997
Volodymyr Kovalchuk <volodymyr.kovalchuk@atos.net>
2019-07-19 16:57:43 +0200

    created input with microphone icon and search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
 
 const App: React.FC = () => {
   return (
-<div class="container-fluid">
-
-</div>
-  );
+    <div className="container main">
+      <div className="row">
+        <div className="col-10">
+          <div className="input-group input-group-lg">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="search for"
+              aria-label="search for"
+              aria-describedby="search for input"
+            />
+            <div className="input-group-append">
+              <button type="button" className="btn btn-light">
+                <FontAwesomeIcon icon={faMicrophone} />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-auto">
+          {' '}
+          <button type="button" className="btn btn-primary btn-lg">
+            Search
+          </button>{' '}
+        </div>
+      </div>
+    </div>
+  )
 }
-
-export default App;
+export default App
